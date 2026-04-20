31ff932cbc78081493c690d7becad2a110e2c001
araae <araliae@icloud.com>
2026-04-20 13:42:27 -0700

    secure the vault

---
+++ index.js
@@ -0,0 +1,9 @@
+let code1;
+code1 = 7 + 3;
+const code2 = 200 / 4 - 10;
+const code3 = (40 % 1) + 39;
+const message = "The vault has been secured. The combination is:";
+const codeA = code1 + "-" + code2 + "-" + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+console.log(message, codeA, codeB);
+//i had to look up 6 and 7 but i understand them now
